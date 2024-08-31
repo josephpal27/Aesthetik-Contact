d97e2a9155262686fcd449910e805ffcbbfea498
Joseph Pal <josephpal27@gmail.com>
2024-08-31 13:09:19 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+
+// Functionality For Footer Section
+let footIconBox = document.querySelectorAll('.footer-icon-row .footer-icon');
+let footIcon = document.querySelectorAll('.footer-icon-row .footer-icon i');
+
+footIconBox.forEach((box, i) => {
+    box.addEventListener('mouseenter', () => {
+        footIcon[i].style.color = '#f15c06';
+    })
+    box.addEventListener('mouseleave', () => {
+        footIcon[i].style.color = '#fff';
+    })
+})
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 // Functionality For Back to Top Btn
 let backToTopDiv = document.querySelector(".back-to-top");
 let backToTopBtn = document.querySelector(".back-to-top .fa-arrow-up");
